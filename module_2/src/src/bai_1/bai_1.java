a3ed1a17b349116a688aec45b62f6e8d15f1c5f7
thanhdn2016 <126248021+thanhdn2016@users.noreply.github.com>
2023-08-07 19:57:33 +0700

    nop bai

--- module_2/src/src/bai_1/bai_1.java
+++ module_2/src/src/bai_1/bai_1.java
@@ -1,7 +1,10 @@
 package bai_1;
-
+import java.util.Scanner;
 public class bai_1 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập vào tên của bạn: ");
+        String name = scanner.nextLine();
+        System.out.println("Xin Chào " + name);
     }
 }
