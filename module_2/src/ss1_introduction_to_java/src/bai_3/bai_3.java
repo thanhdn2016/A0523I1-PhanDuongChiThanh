8a1cba717e1c1e260aa00870f2d6c11caff1c047
thanhdn2016 <126248021+thanhdn2016@users.noreply.github.com>
2023-08-07 21:23:32 +0700

    nop bai

--- module_2/src/ss1_introduction_to_java/src/bai_3/bai_3.java
+++ module_2/src/ss1_introduction_to_java/src/bai_3/bai_3.java
@@ -1,2 +1,14 @@
-package bai_3;public class bai_3 {
+package bai_3;
+
+import java.util.Scanner;
+
+public class bai_3 {
+    public static void main(String[] args) {
+        double rate = 23000;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap vao USD: ");
+         double usd = Float.parseFloat(scanner.nextLine());
+         double quyDoi = usd * rate;
+        System.out.println("Gia tri da quy doi tu USD sang VND la: "+quyDoi+" VND");
+    }
 }
