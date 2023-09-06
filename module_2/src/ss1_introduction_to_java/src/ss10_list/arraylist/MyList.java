c69cdf03a17ebcb83ba753e9e28b7d94b12eea04
thanhdn2016 <126248021+thanhdn2016@users.noreply.github.com>
2023-09-06 20:52:27 +0700

    nop bai

--- module_2/src/ss1_introduction_to_java/src/ss10_list/arraylist/MyList.java
+++ module_2/src/ss1_introduction_to_java/src/ss10_list/arraylist/MyList.java
@@ -1,50 +1,66 @@
 package ss10_list.arraylist;
 
+import java.util.Arrays;
+
 public class MyList<E> {
     int size = 0;
     static final int DEFAULT_CAPACITY = 10;
     Object[] elements;
 
     public MyList() {
-    }
-
-    E get(int i) {
-        Object element = elements[i];
-        return (E)elements[i];
+        elements = new Object[DEFAULT_CAPACITY];
     }
 
     public MyList(int capacity) {
         if (capacity >= 0) {
             this.elements = new Object[capacity];
         } else {
-            System.out.println("Nhap so lon hon 0");
+            System.out.println("Nhap do dai lon hon 0");
         }
+    }
+
+    private void ensureCapa() {
+        int newSize = elements.length * 2;
+        elements = Arrays.copyOf(elements, newSize);
     }
 
     @Override
     public String toString() {
         for (int i = 0; i < elements.length; i++) {
             if (elements[i] != null) {
-                System.out.println(elements[i]);
+                System.out.println("index " + i + ": " + elements[i]);
             }
         }
         return "";
     }
 
     void add(int index, Object element) {
+        if (size == elements.length) {
+            ensureCapa();
+        }
         if (index >= 0 && index < elements.length) {
             elements[index] = element;
+            size++;
         } else {
             System.out.println("index khong hop le");
         }
     }
-//
-//    public E remove(int index) {
-//        for (int i = 0; i < elements.length; i++) {
-//            if (elements.get(i) == index) {
-//
-//            }
-//        }
-//    }
+
+    public E get(int i) {
+        if (i >= size || i < 0) {
+            throw new IndexOutOfBoundsException("Index: " + i + ", Size " + i);
+        }
+        return (E) elements[i];
+    }
+
+    public E remove(int index) {
+        E element = (E) elements[index];
+        for (int i = index; i < size; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[size - 1] = null;
+        size--;
+        return element;
+    }
 
 }
--- module_2/src/ss1_introduction_to_java/src/ss10_list/arraylist/MyListTest.java
+++ module_2/src/ss1_introduction_to_java/src/ss10_list/arraylist/MyListTest.java
@@ -6,12 +6,21 @@ public class MyListTest {
     public static void main(String[] args) {
         MyList<MyList> test = new MyList<>(10);
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhap index muon chen:");
+        test.add(0,"test");
+        test.add(1,"test1");
+        test.add(2,"test2");
+        test.add(3,"test3");
+        System.out.println(test);
+        System.out.print("Nhap vi tri index muon chen:");
         int index = Integer.parseInt(scanner.nextLine());
-        System.out.print("Nhap elements:");
+        System.out.print("Nhap noi dung:");
         Object elements = scanner.nextLine();
         test.add(index,elements);
         System.out.println(test);
-
+        System.out.println("Nhap vi tri index muon xoa");
+        int deleteIndex = Integer.parseInt(scanner.nextLine());
+        test.remove(deleteIndex);
+        System.out.println(test);
+        System.out.println(test.get(2));
     }
 }
