2a38263daf259d0f4da0a6710f6b225d509d5211
thanhdn2016 <126248021+thanhdn2016@users.noreply.github.com>
2023-09-18 20:54:54 +0700

    nop bai

--- module_2/src/ss1_introduction_to_java/src/ss14_thuat_toan_sap_xep/BubbleSort.java
+++ module_2/src/ss1_introduction_to_java/src/ss14_thuat_toan_sap_xep/BubbleSort.java
@@ -1,7 +1,33 @@
 package ss14_thuat_toan_sap_xep;
 
-public class BubbleSort {
-    public void bubbleSort() {
+import java.util.Arrays;
 
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int temp;
+        boolean flag = true;
+        for (int i = 0; i < arr.length && flag; i++) {
+            flag = false;
+            for (int j = arr.length - 1; i < j; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    flag = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{10,5,3,8,4,9};
+        int[] arr2 = new int[]{2,3,5,8,10};
+        System.out.println(Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+        System.out.println("-----------");
+        System.out.println(Arrays.toString(arr2));
+        bubbleSort(arr2);
+        System.out.println(Arrays.toString(arr2));
     }
 }
