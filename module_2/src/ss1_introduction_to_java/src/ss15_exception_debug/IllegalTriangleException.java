d351f24069f1e51fac2529cb7ec2aa08a522ea1a
thanhdn2016 <126248021+thanhdn2016@users.noreply.github.com>
2023-09-22 21:28:16 +0700

    nop bai

--- module_2/src/ss1_introduction_to_java/src/ss15_exception_debug/IllegalTriangleException.java
+++ module_2/src/ss1_introduction_to_java/src/ss15_exception_debug/IllegalTriangleException.java
@@ -1,7 +1,7 @@
 package ss15_exception_debug;
 
 public class IllegalTriangleException extends Exception {
-    public IllegalTriangleException(String message){
+    public IllegalTriangleException(String message) {
         super(message);
     }
 }
