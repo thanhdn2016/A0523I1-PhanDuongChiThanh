30276f908bde8b77a0ec4b869ded9b385df1041d
thanhdn2016 <126248021+thanhdn2016@users.noreply.github.com>
2023-10-30 18:44:18 +0700

    nop bai

--- module_3/ss1/student_management.sql
+++ module_3/ss1/student_management.sql
@@ -3,14 +3,37 @@ CREATE TABLE `student_management`.`student` (
   `id` INT primary key,
   `name` VARCHAR(45) ,
   `age` INT,
-  `country` VARCHAR(45)  );
+  `country` VARCHAR(45)  
+  );
 
 CREATE TABLE `student_management`.`class` (
    id INT primary key,
-  `name` VARCHAR(45) );
+  `name` VARCHAR(45) 
+  );
   CREATE TABLE `student_management`.`teacher` (
    id INT primary key,
   `name` VARCHAR(45) ,
   age INT ,
-  country varchar(50) );
-  
+  country varchar(50) 
+  );
+
+SELECT * FROM student_management.class;
+INSERT INTO `student_management`.`class` (
+`id`, `name`) 
+
+VALUES (
+'1', 'test'
+);
+INSERT INTO `student` (`id`,`name`,`age`,`country`)
+VALUES 
+		(1, 'test 1' , 10, 'test'),
+		(2, 'test 2' , 20, 'test2'),
+		(3, 'test 3' , 30, 'test3');
+	SELECT  *  FROM `student`;
+insert into teacher (id,`name`,age,country)
+values
+			(1,'test 1' , 10, 'test'),
+			(2, 'test 2', 20, 'test 2');
+            select * from teacher;
+
+
